f299cb738a5d704979a78f804c541358507b25d6
William Woodruff <william@tuffbizz.com>
2015-09-26 19:16:29 -0400

    no commit msg

--- src/client/main.c
+++ src/client/main.c
@@ -5,14 +5,26 @@
  */
 
 #include <avr/io.h>
+#include "m_general.h"
 #include "maevarm-rf.h"
+#include "m_usb.h"
 
 int main(void)
 {
+	// unsigned int value;
+
 	/* insert your hardware initialization here */
+	m_usb_init();
+
+	while (!m_usb_isconnected());
 
 	for(;;) {
-		/* insert your main loop code here */
+		// if (m_usb_rx_available()) {
+		// 	value = m_usb_rx_char();
+		// 	m_usb_tx_uint(value);
+		// }
+		m_wait(500);
+		m_green(TOGGLE);
 	}
 
 	return 0;   /* never reached */
